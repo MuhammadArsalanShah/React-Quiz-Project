a415f5cfe8a4923b5cc2c942235df197312b8d16
MuhammadArsalanShah <arsalanshah408@yahoo.com>
2024-09-24 14:29:14 +0500

    Using effect cleanup function and fixing the interval and timer for each question

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,18 +1,29 @@
 import { useEffect, useState } from "react";
 
 export default function QuestionTimer({ timeout, onTimeout }) {
+
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
     console.log('Setting TIMEOUT');
-    setTimeout(onTimeout, timeout);
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => {
+      console.log('Timeout CLEARED')
+      clearTimeout(timer);
+    }
   }, [timeout, onTimeout])
 
   useEffect(() => {
     console.log('Setting INTERVAL');
-    setInterval(() => {
+    const interval = setInterval(() => {
       setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
+
+    return () => {
+      console.log('Interval CLEARED')
+      clearInterval(interval);
+    }
   }, []);
 
   return <progress id="question-time" max={timeout} value={remainingTime} />;
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -33,11 +33,12 @@ export default function Quiz() {
 
   return (
     <div id="quiz">
+      <div id="question">
       <QuestionTimer
+        key={activeQuestionIndex}
         timeout={10000}
         onTimeout={handleSkipAnswer}
       />
-      <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnwers.map((answer) => (
